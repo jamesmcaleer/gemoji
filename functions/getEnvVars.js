3a6200652467ec7853e66b3356c498bb4e7f0832
James McAleer <77809943+jamesmcaleer@users.noreply.github.com>
2023-07-31 14:13:24 -0400

    Update getEnvVars.js

--- functions/getEnvVars.js
+++ functions/getEnvVars.js
@@ -1,10 +1,19 @@
-exports.handler = function (event, context, callback) {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      apiKey: process.env.API_KEY,
-    }),
-  };
+exports.handler = async function (event, context) {
+  try {
+    // Perform any logic or data retrieval here to get the environment variables
+    const apiKey = process.env.API_KEY; // Replace 'API_KEY' with your actual environment variable name
 
-  callback(null, response);
+    // Return the environment variables as JSON response
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ apiKey }),
+    };
+  } catch (error) {
+    console.error('Error fetching environment variables:', error);
+    // Return an error response if there was an issue retrieving the environment variables
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal Server Error' }),
+    };
+  }
 };
